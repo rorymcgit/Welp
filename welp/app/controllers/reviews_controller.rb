c36d2b0d5cf24d1f0d916cec2cb0f6091e6a7dfc
Christos <chris.paraskeva10@gmail.com>
2017-04-06 18:14:37 +0100

    user cannot leave more than one review for a specific restaurant

--- welp/app/controllers/reviews_controller.rb
+++ welp/app/controllers/reviews_controller.rb
@@ -1,10 +1,14 @@
 class ReviewsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.create(review_params)
-    p current_user
-    @review.update(user_id: current_user.id)
-    redirect_to restaurant_path(@restaurant)
+
+    if Review.find_by(user_id: current_user.id, restaurant_id: @restaurant.id) == nil
+      @review = @restaurant.reviews.create(review_params)
+      @review.update(user_id: current_user.id)
+      redirect_to restaurant_path(@restaurant)
+    else
+      redirect_to restaurant_path(@restaurant)
+    end
   end
 
   private
