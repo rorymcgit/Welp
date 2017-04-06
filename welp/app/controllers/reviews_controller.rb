b9932931c8ef90ccee1172e58a320a54b52e8e9c
Rory McGuinness <rorymcguinness@hotmail.co.uk>
2017-04-06 11:24:50 +0100

    Can add review in UI. Reviews displaying

--- welp/app/controllers/reviews_controller.rb
+++ welp/app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.create(review_params)
+
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating)
+  end
 end
