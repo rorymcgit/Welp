4fe9793536caa96e0bb9029fbc1ff059ff797e58
Christos <chris.paraskeva10@gmail.com>
2017-04-06 18:42:51 +0100

    refactored method to check if user has left existing review for a specific restaurant

--- welp/app/controllers/reviews_controller.rb
+++ welp/app/controllers/reviews_controller.rb
@@ -5,11 +5,9 @@ class ReviewsController < ApplicationController
     if Review.find_by(user_id: current_user.id, restaurant_id: @restaurant.id) == nil
       @review = @restaurant.reviews.create(review_params)
       @review.update(user_id: current_user.id)
-      redirect_to restaurant_path(@restaurant)
-    else
+    end
       redirect_to restaurant_path(@restaurant)
     end
-  end
 
   private
   def review_params
